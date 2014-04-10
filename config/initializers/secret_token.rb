a8beb2cf6c15d410394c958e062c106b6778352b
Michael Orr <michael@cloudspace.com>
2014-04-10 17:42:08 +0000

    initial project generation using the Cloudspace Rails 4 Template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splitstack8::Application.config.secret_key_base = 'cf9e7b61979f4935c682e54d38c37bfc60da15ae9f5a6013d8aaba55482d89ec057c31548b34c47be61238df2f847c72cc180872e068d1501bec6626c693adf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Splitstack8::Application.config.session_store :cookie_store, key: '_splitstack8_session', domain: 'splitstack8.com'
